c228896d73d1e17f236b3633e1a8bd55429295d3
morsk <harohermsdorf@web.de>
2024-11-24 21:43:40 +0100

    Update battery_status.c

--- boards/shields/dongle_display/widgets/battery_status.c
+++ boards/shields/dongle_display/widgets/battery_status.c
@@ -99,8 +99,8 @@ int zmk_widget_peripheral_battery_status_init(struct zmk_widget_peripheral_batte
 
         lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 5, 8, LV_IMG_CF_TRUE_COLOR);
 
-        lv_obj_align(image_canvas, LV_ALIGN_TOP_RIGHT, -2, i * 10);
-        lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -9, i * 10);
+        lv_obj_align(image_canvas, LV_ALIGN_TOP_RIGHT, -1, i * 10);
+        lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -8, i * 10);
     }
 
     sys_slist_append(&widgets, &widget->node);
