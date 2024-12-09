524cd127bcc39859603ad3667436f127cea201a3
morsk <harohermsdorf@web.de>
2024-12-09 17:25:42 +0100

    Update custom_status_screen.c

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -20,8 +20,8 @@ static struct zmk_widget_output_status output_status_widget;
 static struct zmk_widget_layer_status layer_status_widget;
 static struct zmk_widget_peripheral_battery_status peripheral_battery_status_widget;
 static struct zmk_widget_modifiers modifiers_widget;
-static struct zmk_widget_bongo_cat bongo_cat_widget;
-static struct zmk_widget_sunfire sunfire_widget;
+//static struct zmk_widget_bongo_cat bongo_cat_widget;
+
 
 #if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
 static struct zmk_widget_hid_indicators hid_indicators_widget;
